fba3a178ad229d24b602fc296c13e945ce2b25a3
Antun Flaš <antun.flas@gmail.com>
2024-12-24 10:20:06 +0100

    Update generate_changelog step to include unlabeled pull requests without skip-changelog

--- .github/actions/generate_changelog/index.js
+++ .github/actions/generate_changelog/index.js
@@ -16,14 +16,22 @@ async function run() {
 
     // Filter out PRs with the label "skip-changelog"
     const filteredPrDetails = prDetails.filter(pr => !pr.labels.includes('skip-changelog'));
+    const breakingChangePrs = filteredPrDetails.filter(pr => pr.labels.includes('breaking-change'));
+    const newFeaturePrs = filteredPrDetails.filter(pr => pr.labels.includes('new-feature'));
+    const bugfixPrs = filteredPrDetails.filter(pr => pr.labels.includes('bugfix'));
+
+    // Compute otherChangesPrs based on the filtered PRs
+    const otherChangesPrs = filteredPrDetails.filter(pr =>
+      !breakingChangePrs.includes(pr) &&
+      !newFeaturePrs.includes(pr) &&
+      !bugfixPrs.includes(pr)
+    );
 
     let changelog = '';
-    const breakingChanges = filteredPrDetails.filter(pr => pr.labels.includes('breaking-change'))
-      .map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
-    const newFeatures = filteredPrDetails.filter(pr => pr.labels.includes('new-feature'))
-      .map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
-    const bugfixes = filteredPrDetails.filter(pr => pr.labels.includes('bugfix'))
-      .map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
+    const breakingChanges = breakingChangePrs.map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
+    const newFeatures = newFeaturePrs.map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
+    const bugfixes = bugfixPrs.map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
+    const otherChanges = otherChangesPrs.map(pr => `- ${pr.title} by @${pr.author} in #${pr.number}`).join('\n');
 
     if (breakingChanges) {
       changelog += '### Breaking Changes\n' + breakingChanges + '\n\n';
@@ -33,6 +41,11 @@ async function run() {
     }
     if (bugfixes) {
       changelog += '### Bug Fixes\n' + bugfixes + '\n\n';
+    }
+    if (otherChanges && !breakingChanges && !newFeatures && !bugfixes) {
+      changelog += '### Changes\n' + otherChanges + '\n\n';
+    } else if (otherChanges) {
+      changelog += '### Other Changes\n' + otherChanges + '\n\n';
     }
 
     changelog += `\n\n**Full Changelog**: [v${currentVersion}...v${newVersion}](https://github.com/${context.repo.owner}/${context.repo.repo}/compare/v${currentVersion}...v${newVersion})\n`;
